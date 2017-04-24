278e34840c4a51fc63b0b283413077be465cca5e
seanyboyswag <2150218@slu.edu.ph>
2017-04-24 13:29:04 +0800

    Made datatables' rows striped and added icons

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,7 +14,7 @@
                         {{--</div>--}}
                         {{--You are logged in!--}}
 
-                        <table id="studentsTable" class="table">
+                        <table id="studentsTable" class="table table-striped">
                             <thead>
                             <tr>
                                 <th>Id Number</th>
@@ -69,7 +69,7 @@
                     <div class="panel-heading">Signed In Students</div>
 
                     <div class="panel-body">
-                        <table id="signedInTable" class="table">
+                        <table id="signedInTable" class="table table-striped">
                             <thead>
                             <tr>
                                 <th>Id Number</th>
